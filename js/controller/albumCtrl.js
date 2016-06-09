4d78e7f05995be70ee61550fda48f6d86ed8b41b
xjun13 <xjunsantos@gmail.com>
2016-06-09 19:22:23 +0800

    .

--- js/controller/albumCtrl.js
+++ js/controller/albumCtrl.js
@@ -7,30 +7,39 @@ app.controller('albumsCtrl', function($scope, songsAPI) {
 	$scope.showAdd = false;
 	var maxSecs = 0;
 	var totalListenedSecs = 0;
+
 	$scope.allSongs = [];
+
 	// get songs from API
 	songsAPI.getSongs().then(function (response) {
 		$scope.allSongs = response.data[0].songs;
 	});
 
-	$scope.album1 = [ 
-				{title: 'Title1', duration: 10},
-				{title: 'Title2', duration: 20},
-				{title: 'Title3', duration: 30}
-		];	
-
-	$scope.album2 = [ 
-				{title: 'Title4', duration: 40},
-				{title: 'Title5', duration: 50},
-				{title: 'Title6', duration: 60}
-		];	
-
+	$scope.albums = [ 
+						{	name: 'Album1', 
+							artist: 'Random', 
+							cat: 'Pop',
+							songs: [ {title: 'Title1', duration: 10},
+									 {title: 'Title2', duration: 20},
+									 {title: 'Title3', duration: 30} 
+								   ]
+						},
+						{ 	name: 'Album2', 
+							artist: 'Random', 
+							cat: 'Rock',
+							songs: [ 
+										{title: 'Title4', duration: 40},
+										{title: 'Title5', duration: 50},
+										{title: 'Title6', duration: 60}
+								   ]
+						}
+				];
+	
 	//watch
 	$scope.$watch('maxMins', function(newValue, oldValue) {
 		//console.log(newValue, oldValue);
 	});
 
-	
 	$scope.s1 = {
 		show: false,
 		showAdd: function () {
@@ -40,14 +49,14 @@ app.controller('albumsCtrl', function($scope, songsAPI) {
 
 	$scope.s2 = angular.copy($scope.s1);
 
-	// Album Obj	
+	// album obj	
 	$scope.albumsObj = {
 		songs: {},
 		remaingSongs: [],
 
+		// put song duration in an array remainingSongs[] and sort it by ASC order
 		setSongs: function (songs) {
 			this.songs = songs;
-			// Put song durations in an array and sort it by ASC order
 			var arr = [];
 			angular.forEach(this.songs, function(song) {
 				arr.push(song.duration);
@@ -56,97 +65,102 @@ app.controller('albumsCtrl', function($scope, songsAPI) {
 			this.remaingSongs = arr;
 		},
 
+		// listened to the required song and remove it in the array remainingSongs[]
 		listenReqSongs: function (req) {
+			// check if there is enough song if not return false
 			if(this.remaingSongs.length<req) return false;
 			var listenedSecs = 0;
 			for(var x=0;x<req;x++) {
 				listenedSecs = listenedSecs + parseInt(this.remaingSongs[x]);
 				this.songsListened++;
-				// remove songs already listened
 				this.removeSong(this.remaingSongs[x]);
 			}
 			return listenedSecs;
 		},
 
+		// remove a song already listened
 		removeSong: function(v) {
 			var index = this.remaingSongs.indexOf(v);
 			this.remaingSongs.splice(index, 1);
 		},
 
-		getFirstSong: function() {
+		// check if there still remaingSongs songs
+		getSong: function() {
 			if(this.remaingSongs.length == 0) return false;
 			return this.remaingSongs[0];
 		},
 
+		// listen to a song and remove it in the array remainingSongs[]
 		listen: function() {
-			var song = this.getFirstSong();
+			var song = this.getSong();
 			this.removeSong(song);
 			return parseInt(song);
 		}
 
 	} 	
 
+	// check if the total listened is already in max
 	var is_exceed = function() {
 		if(totalListenedSecs >= maxSecs) return 1;
 		return 0;
 	}
 
-
+	// add a song to listen if still not in max
 	var addSong = function() {
 		var no_songs = false;
+		var song1 = albumObj1.getSong();
+		var song2 = albumObj1.getSong();
 
-		if(albumObj1.remaingSongs.length == 0 && albumObj2.remaingSongs.length == 0 ) {
-			no_songs = true;
-		} else if(albumObj1.remaingSongs.length > 0 && albumObj2.remaingSongs.length == 0 ) {
-			totalListenedSecs = totalListenedSecs + albumObj1.listen();
-		} else if(albumObj2.remaingSongs.length > 0  && albumObj1.remaingSongs.length == 0) {
-			totalListenedSecs = totalListenedSecs + albumObj2.listen();
-		} else if(albumObj1.remaingSongs[0] <= albumObj2.remaingSongs[0]) {
-			totalListenedSecs = totalListenedSecs + albumObj1.listen();
+		if(song1 && song2) {
+			totalListenedSecs += (song1 <= song2) ?  albumObj1.listen() : albumObj2.listen();
+		} else if(song1 && !song2 ) {
+			totalListenedSecs += albumObj1.listen();
+		} else if(!song1 && song2 ) {
+			totalListenedSecs += albumObj2.listen();
 		} else {
-			totalListenedSecs = totalListenedSecs + albumObj2.listen();
+			no_songs = true;
 		}
 
+		// add a song if there still available and still not exceed the max time
 		if(!is_exceed()) {
 			if(!no_songs) {
-				//console.log(totalListenedSecs);
 				$scope.songsListened++;
 				addSong();
 			}
 		}
 
+		// return the total number of songs listened
 		return ($scope.reqSongs * 2) + $scope.songsListened;
-		//console.log(totalListenedSecs);
 	}	
 
 	var albumObj1 = $scope.albumsObj;
 	var albumObj2 = angular.copy(albumObj1);
 
-
 	$scope.main = function() {
+
 		totalListenedSecs = 0;
 		$scope.songsListened = 0;
 		maxSecs = $scope.maxMins * 60;
 
-		// set songs both album
-		albumObj1.setSongs($scope.album1);
-		albumObj2.setSongs($scope.album2);
+		albumObj1.setSongs($scope.albums[0].songs);
+		albumObj2.setSongs($scope.albums[1].songs);
 
-		var a1 = albumObj1.listenReqSongs($scope.reqSongs);
-		var a2 = albumObj2.listenReqSongs($scope.reqSongs);
+		// check if min required songs is pass if yes return total songs listened in secs
+		var passReq1 = albumObj1.listenReqSongs($scope.reqSongs);
+		var passReq2 = albumObj2.listenReqSongs($scope.reqSongs);
 
-		if(a1 && a2) {
-			totalListenedSecs = a1 + a2;
+		if(passReq1 && passReq2) {
+			totalListenedSecs = passReq1 + passReq2;
+			// check if already exceed the max time
 			if(!is_exceed()) {
 				$scope.songsListened = addSong();	
+				console.log($scope.songsListened);
 			} else {
 				$scope.songsListened = -1;
 			}
 		} else {
 			$scope.songsListened = -1;
 		}
-
-		//console.log($scope.songsListened);
 	}
 
 });
